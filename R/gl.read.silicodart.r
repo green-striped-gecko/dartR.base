bd941f2fb317646d006ee64a95f8c8d4c158272b
Bernd.Gruber <Bernd.Gruber@canberra.edu.au>
2023-06-02 16:56:31 +1000

    fixed length example line

--- R/gl.read.silicodart.r
+++ R/gl.read.silicodart.r
@@ -53,7 +53,8 @@
 #'  \url{https://groups.google.com/d/forum/dartr}
 #' @examples
 #' silicodartfile <- system.file('extdata','testset_SilicoDArT.csv', package='dartR.data')
-#' metadata <- system.file('extdata',ind.metafile ='testset_metadata_silicodart.csv', package='dartR.data')
+#' metadata <- system.file('extdata',ind.metafile ='testset_metadata_silicodart.csv',
+#' package='dartR.data')
 #' testset.gs <- gl.read.silicodart(filename = silicodartfile, ind.metafile = metadata)
 #' @seealso \code{\link{gl.read.dart}}
 
