04e38d207e27b5ff7eb46547ec61d9012ea15711
Carlo Pacioni (DEECA) <carlo.pacioni@delwp.vic.gov.au>
2023-07-17 09:49:16 +1000

    first draft gl2BayesAss

---
+++ R/gl2BayesAss.R
@@ -0,0 +1,85 @@
+#' @name gl2bayesAss
+#' @title Converts a genlight object into bayesAss (BA3) input format
+#' @family linker
+#' 
+#' @description
+#' This function exports a genlight object into bayesAss format and save it into a
+#' file.
+#' This function only caters for \code{ploidy=2}.
+#' 
+#' @param x Name of the genlight object containing the SNP data [required].
+#' @param outfile File name of the output file [default 'gl.BayesAss.txt'].
+#' @param outpath Path where to save the output file [default global working 
+#' directory or if not specified, tempdir()].
+#' @param verbose Verbosity: 0, silent or fatal errors; 1, begin and end; 2,
+#' progress log; 3, progress and results summary; 5, full report
+#' [default 2 or as specified using gl.set.verbosity].
+#' 
+#' @author Custodian: Carlo Pacioni (Post to
+#'  \url{https://groups.google.com/d/forum/dartr})
+#'  
+#' @examples
+#' \donttest{
+#' require("dartR.data")
+#' gl2bayesAss(platypus.gl)
+#' }
+#' @references
+#' 
+#' 
+#' @export
+#' @return  returns the input file as data.table
+gl2bayesAss <-  function(x, 
+                         ploidy=2, 
+                         outfile="gl.BayesAss.txt", 
+                         outpath=NULL,
+                         verbose=NULL) {
+  # SET VERBOSITY
+  verbose <- gl.check.verbosity(verbose)
+  
+  # SET WORKING DIRECTORY
+  outpath <- gl.check.wd(outpath,verbose=0)
+  
+  # FLAG SCRIPT START
+  funname <- match.call()[[1]]
+  utils.flag.start(func = funname,
+                   build = "v.2023.2",
+                   verbosity = verbose)
+  
+  # CHECK DATATYPE
+  datatype <- utils.check.datatype(x, verbose = verbose)
+  
+  # FUNCTION SPECIFIC ERROR CHECKING
+  if(ploidy != 2) stop("This function only caters for ploidy=2")
+  
+  # DO THE JOB
+  m <- as.matrix(x)
+  dtref<- data.table(c(0,1,2,NA), c(1,1,2,0), c(1,2,2,0))
+  mdt <- data.table(m, keep.rownames = T)
+  
+  byLoc <- function(i, mdt, dtref) {
+    loc <- names(mdt)[1 + i]
+    mdtsub <- mdt[,c("rn", loc), with=F]
+    setnames(mdtsub, loc, "locus")
+    mdtsub[dtref, on=c(locus="V1"), c("All1", "All2") := list(i.V2, i.V3)]
+    mdtsub[, locus := NULL]
+    mdtsub[, Locus := loc]
+    mdtsub[, Pop := pop(x)]
+    setcolorder(mdtsub, c("rn", "Pop", "Locus", "All1", "All2"))
+    return(mdtsub)
+  }
+  
+  l <- lapply(seq_len(ncol(m)), byLoc, mdt, dtref)
+  res <- rbindlist(l)
+  setkeyv(res, cols=c("Pop", "rn"))
+  write.table(res, file.path(outpath, outfile), 
+              quote = FALSE, row.names = FALSE, col.names = FALSE)
+  
+  # FLAG SCRIPT END
+  
+  if (verbose > 0) {
+    cat(report("Completed:", funname, "\n"))
+  }
+  
+  
+  return(invisible(res))
+  }
