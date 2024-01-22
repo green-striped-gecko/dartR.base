a7d993c21b3dab602632dfeb60ef1f1a58514e47
Luis Mijangos <mijangos@Luiss-MBP.diversityarrays.com>
2024-01-22 11:55:47 +1100

    fix snp position in gl.compliance.check

--- R/gl.compliance.check.r
+++ R/gl.compliance.check.r
@@ -278,6 +278,16 @@ gl.compliance.check <- function(x,
         ))
     }
     
+    # Check SNP position
+    if(is.null(x$position) & "SnpPosition" %in% names(x$other$loc.metrics)){
+      x$position <- x$other$loc.metrics$SnpPosition
+      if (verbose >= 2) {
+        cat(report(
+          "  Assigning SNP position using slot gl$other$loc.metrics$SnpPosition \n"
+        ))
+      }
+    }
+    
     # ADD TO HISTORY
     nh <- length(x@other$history)
     x@other$history[[nh + 1]] <- match.call()
