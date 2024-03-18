77e7e567804081d2ccd28b226f51cb055220e8fa
Arthur Georges <arthur.georges@biomatix.com.au>
2024-03-18 17:12:01 +1100

    Minor errors not liked by build

--- R/gl.diagnostics.hwe.r
+++ R/gl.diagnostics.hwe.r
@@ -82,12 +82,12 @@
 #' }
 #' @author Custodian: Carlo Pacioni -- Post to
 #'   \url{https://groups.google.com/d/forum/dartr}
-#' @examples
-#' \donttest{
-#' require("dartR.data")
-#' res <- gl.diagnostics.hwe(x = gl.filter.allna(platypus.gl[,1:50]), 
-#' stdErr=FALSE, n.cores=1)
-#' }
+# @examples
+# \donttest{
+# require("dartR.data")
+# res <- gl.diagnostics.hwe(x = gl.filter.allna(platypus.gl[,1:50]), 
+# stdErr=FALSE, n.cores=1)
+# }
 #' @references \itemize{ 
 #' \item de Meeûs, T., McCoy, K.D., Prugnolle, F.,
 #' Chevillon, C., Durand, P., Hurtrez-Boussès, S., Renaud, F., 2007. Population
