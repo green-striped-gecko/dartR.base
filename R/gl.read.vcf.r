1ef7a8bb1eca23afea03a8587be8259d8aed4114
Ching Ching Lau <chingchinglau@Chings-MacBook-Air.local>
2024-08-28 15:48:02 +1000

    add parameter documentation

--- R/gl.read.vcf.r
+++ R/gl.read.vcf.r
@@ -7,6 +7,9 @@
 #' @param verbose Verbosity: 0, silent or fatal errors; 1, begin and end; 2,
 #' progress log; 3, progress and results summary; 5, full report
 #' [default 2, unless specified using gl.set.verbosity].
+#' @param mode genotype: all heterozygous sites will be coded as 1 regardless ploidy level, 
+#' dosage: sites will be codes as copy number of alternate allele
+#' [default 2, unless specified using gl.set.verbosity].
 #' @details
 #' The ind.metadata file needs to have very specific headings. First a heading
 #' called id. Here the ids have to match the ids in the dartR object. 
