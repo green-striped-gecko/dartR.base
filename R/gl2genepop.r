d7c5fe8aed044d2d70f6513fc987701294b4de2a
Bernd.Gruber <Bernd.Gruber@canberra.edu.au>
2025-03-04 12:11:23 +1100

    fixed examples gl.report.ld.map still has a bug

--- R/gl2genepop.r
+++ R/gl2genepop.r
@@ -4,8 +4,8 @@
 
 #' @description
 #' The genepop format is used by several external applications (for example
-#' Neestimator2
-#' (\url{http://www.molecularfisherieslaboratory.com.au/neestimator-software/}).
+#' Neestimator2. Unfortunatelly, the software seems to be no longer easily 
+#' available. To install use the \link[dartRverse]{gl.download.binary} function.
 #' So the main idea is to create the genepop file and then run the other
 #' software externally. As a feature, the genepop file is also returned as an
 #' invisible data.frame by the function.
